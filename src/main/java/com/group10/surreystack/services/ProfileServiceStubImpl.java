d5cb8f747a1ac62745772109846ef2da58a303ae
Ignas Kampas <ikampas@yahoo.co.uk>
2018-11-20 21:33:46 +0000

    User can view his/her username and name in profile page

---
+++ src/main/java/com/group10/surreystack/services/ProfileServiceStubImpl.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.group10.surreystack.services;
+
+import com.group10.surreystack.models.User;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author ikamp
+ */
+@Service
+public class ProfileServiceStubImpl implements ProfileService{
+
+    private User user = new User(5L, "username1", "Ignas Kampas");
+    
+    @Override
+    public String getUsername() {
+        return this.user.getUsername();
+    }
+
+    @Override
+    public String getFullName() {
+        return this.user.getFullName();
+    }
+    
+}
