853c155832106855117354630c026cce4f44d329
Liam Kenny <lck3nny@gmail.com>
2018-11-22 16:33:27 +0000

    added fields to register form

--- src/main/java/com/group10/surreystack/forms/RegisterForm.java
+++ src/main/java/com/group10/surreystack/forms/RegisterForm.java
@@ -5,6 +5,7 @@
  */
 package com.group10.surreystack.forms;
 
+import java.util.Date;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
@@ -13,12 +14,27 @@ import javax.validation.constraints.Size;
  * @author aruns
  */
 public class RegisterForm {
+    @NotNull
     @Size(min=5, max=30, message = "Username size should be in the range [5...30]")
     private String username;
 
     @NotNull
     @Size(min=1, max=50)
     private String password;
+    
+    @NotNull
+    @Size(min=1, max=50)
+    private String name;
+    
+
+    
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 
     public String getUsername() {
         return username;
