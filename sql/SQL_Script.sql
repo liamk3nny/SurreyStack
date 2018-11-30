5478e9e5c0bcda9b6feabd8423dde9dcaf599ec7
Liam Kenny <lck3nny@gmail.com>
2018-11-30 17:06:34 +0000

    added sql init file

---
+++ sql/SQL_Script.sql
@@ -0,0 +1,19 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ * Author:  liamkenny
+ * Created: 30-Nov-2018
+ */
+
+create database db_surreystack;
+
+CREATE TABLE `tblUser` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(45) NOT NULL,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(45) NOT NULL,
+  PRIMARY KEY (`user_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
